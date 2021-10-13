86bf755184081ef75f07459adeb9d29de8f30018
mkhan503 <mahah.mujeebkhan@gmail.com>
2021-10-13 09:55:20 +0400

    Palindrome solution, in pseudocode

---
+++ palindromes.js
@@ -0,0 +1,14 @@
+const palindromes = function (expression) {
+    //split the expression into an array. Including characters and white space
+    let arr1 = expression.split("");
+
+    //remove whitespace and characters from string
+
+    //create a new array reversed
+
+    //use array method to compare the two arrays. 
+    
+
+};
+
+module.exports = palindromes;
