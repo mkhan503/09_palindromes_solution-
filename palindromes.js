8f1d08bc4b556c54858058afdbc6c64cb7d30d5c
mkhan503 <mahah.mujeebkhan@gmail.com>
2021-10-15 03:05:40 +0400

    Tests not passing

--- palindromes.js
+++ palindromes.js
@@ -1,14 +1,47 @@
-const palindromes = function (expression) {
+
+const palindromes = function(expression){
     //split the expression into an array. Including characters and white space
-    let arr1 = expression.split("");
-
-    //remove whitespace and characters from string
-
-    //create a new array reversed
-
-    //use array method to compare the two arrays. 
+    let statement = expression.toLowerCase();
     
+    //console.log(statement);
 
-};
+    let arr1 = statement.split("");
+    //console.log(arr1);
+
+    const onlyLetters = arr1.filter(item =>{
+        //check if each elements in array is a white space or not an alphabet
+        const pattern = /[a-z]/i;
+        // true values are kept, false thrown out.
+        return pattern.test(item);
+        
+    });
+
+    //console.log(onlyLetters);
+
+    //create a flipped array
+  
+    let arr2 = []
+    const len = onlyLetters.length - 1;
+    for (let i = len ; i >= 0; i-- ){
+        arr2.push(onlyLetters[i]);
+    }
+
+    //console.log(arr2);
+
+    //checking if both are equal
+    for (i = 0; i <= len; i++ ){
+        if (onlyLetters[i] != arr2 [i]){
+        console.log(onlyLetters[i]);
+        console.log(arr2[i])
+            return false;
+        }
+
+    }
+
+    return true;
+}
+
+
+
 
 module.exports = palindromes;
